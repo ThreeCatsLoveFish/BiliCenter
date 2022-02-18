d4e363c8085ac9029bce01c90d04d30a24ef9ca6
ThreeCats <121097854@qq.com>
2022-02-18 23:54:18 +0800

    [Test] use awpush as main function

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
-	"subcenter/service/task"
+	// "subcenter/service/task"
+	"subcenter/service/awpush"
 )
 
 func main() {
-	tc := task.NewTaskCenter()
-	tc.Run()
+	// tc := task.NewTaskCenter()
+	// tc.Run()
+
+	client := awpush.NewAWPushClient()
+	client.Serve()
 }
