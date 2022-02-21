f6ab4ddd2c9b918c826260a6e26a71948d2da6ea
sunzhimin <sunzhimin@ft.tech>
2022-02-21 17:05:53 +0800

    [Refactor] move config to awpush

--- application/awpush/config.go
+++ application/awpush/config.go
@@ -4,13 +4,34 @@ import (
 	"net/http"
 	"regexp"
 	"strconv"
-	"subcenter/infra/vo"
 
 	"github.com/gookit/config/v2"
 	"github.com/gookit/config/v2/toml"
 )
 
-var biliConfig vo.BiliConfig
+var biliConfig BiliConfig
+
+type User struct {
+	Uid    int32
+	Cookie string `config:"cookie"`
+	Push   string `config:"push"`
+}
+
+// LiveFilter store rooms and words with which lottery won't be join
+type LiveFilter struct {
+	Rooms    []int32  `config:"rooms"`
+	Words    []string `config:"words"`
+	WordsPat []*regexp.Regexp
+}
+
+// BiliConfig store all token and cookies used for awpush and bili live
+type BiliConfig struct {
+	Uid    string     `config:"uid"`
+	Token  string     `config:"token"`
+	Wss    string     `config:"wss"`
+	Users  []User     `config:"users"`
+	Filter LiveFilter `config:"filter"`
+}
 
 func init() {
 	initAWPush()
