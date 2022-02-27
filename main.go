24975ceec697a8a024e219e9a50c14cc74978653
ThreeCats <121097854@qq.com>
2022-02-27 18:22:01 +0800

    [Feat] update main

--- main.go
+++ main.go
@@ -2,23 +2,25 @@ package main
 
 import (
 	"subcenter/application/api"
-	// "subcenter/application/awpush"
-	// "subcenter/domain"
+	"subcenter/application/awpush"
+	"subcenter/application/frontend"
+	"subcenter/domain"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	// Initialize the task center
-	// taskCenter := domain.NewTaskCenter()
-	// go taskCenter.Run()
+	taskCenter := domain.NewTaskCenter()
+	go taskCenter.Run()
 
 	// Initialize awpush client
-	// client := awpush.NewAWPushClient()
-	// go client.Run()
+	client := awpush.NewAWPushClient()
+	go client.Run()
 
 	// Initialize frontend
 	router := gin.Default()
+	frontend.LoadFrontend(router)
 	api.LoadApi(router)
 	router.Run(":8000")
 }
