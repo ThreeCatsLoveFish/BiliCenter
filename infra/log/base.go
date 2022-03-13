c353d6523b6f568b7b3ae097bada2ca92d4bf2f4
ThreeCats <121097854@qq.com>
2022-03-13 16:58:46 +0800

    [Feat] support spilt log file

--- infra/log/base.go
+++ infra/log/base.go
@@ -1,37 +1,11 @@
 package log
 
-import (
-	"fmt"
-	"io"
-	"log"
-	"os"
-)
-
-var logger *log.Logger
+var w *AsyncFileWriter
 
 // Init can initialize default logger
 func init() {
-	logFile, err := os.OpenFile("output/subcenter.log",
-		os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0666)
-	if err != nil {
-		panic("create log file error")
+	w = NewAsyncFileWriter("output/subcenter.log")
+	if err := w.initLogFile(); err != nil {
+		panic(err)
 	}
-	flag := log.Ldate | log.Lmicroseconds | log.Lshortfile
-	logger = log.New(logFile, "", flag)
-}
-
-func Writer() io.Writer {
-	return logger.Writer()
-}
-
-func Debug(format string, v ...interface{}) {
-	logger.Output(2, fmt.Sprintf("[DEBUG] "+format, v...))
-}
-
-func Info(format string, v ...interface{}) {
-	logger.Output(2, fmt.Sprintf("[INFO] "+format, v...))
-}
-
-func Error(format string, v ...interface{}) {
-	logger.Output(2, fmt.Sprintf("[ERROR] "+format, v...))
 }
