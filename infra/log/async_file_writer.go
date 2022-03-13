82afa2127fe41ae70b99ae2cdbfcc6173d11b44b
ThreeCats <121097854@qq.com>
2022-03-13 17:20:30 +0800

    [Fix] remove detail time of log filename

--- infra/log/async_file_writer.go
+++ infra/log/async_file_writer.go
@@ -117,5 +117,5 @@ func (w *AsyncFileWriter) rotateFile() {
 }
 
 func (w *AsyncFileWriter) timeFilePath() string {
-	return w.filePath + "." + time.Now().Format("2006-01-02_15:04")
+	return w.filePath + "." + time.Now().Format("2006-01-02")
 }
