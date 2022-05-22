3887335e5c62a87a6d9eec96089ba8486210639e
ThreeCats <121097854@qq.com>
2022-05-22 12:35:22 +0800

    [Feat] Add api for hadoop hosts info

--- application/api/base.go
+++ application/api/base.go
@@ -4,26 +4,10 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type StatusCode int32
-
-// Status code
-const (
-	Success StatusCode = iota
-	ErrParam
-	ErrInternal
-)
-
-type Resp struct {
-	Code StatusCode  `json:"code"`
-	Msg  string      `json:"msg,omitempty"`
-	Data interface{} `json:"data,omitempty"`
-}
-
 // LoadApi add router for api service
 func LoadApi(router *gin.Engine) {
-	// Push
-	router.GET("/api/push/endpoint/list/", ListEndpoint)
-	router.POST("/api/push/endpoint/update/", UpdateEndpoint)
-	// Bili account
-	router.GET("/api/bili/update/", UpdateBili)
+	// Hadoop Hosts config
+	router.GET("/api/hadoop/hosts/list/", ListHosts)
+	router.POST("/api/hadoop/hosts/update/", UpdateHosts)
+	router.POST("/api/hadoop/hosts/reset/", UpdateHosts)
 }
---
+++ application/api/hosts.go
@@ -0,0 +1,51 @@
+package api
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func init() {
+	hosts = make([]Hosts, 0, 10)
+}
+
+var hosts []Hosts
+
+type Hosts struct {
+	Hostname string `json:"hostname"`
+	IP       string `json:"ip"`
+}
+
+// UpdateHosts update hosts data
+func UpdateHosts(c *gin.Context) {
+	var host Hosts
+	if err := c.ShouldBindJSON(&host); err != nil {
+		c.Data(http.StatusBadRequest, "text/plain", []byte("Wrong data"))
+		return
+	}
+	for i, v := range hosts {
+		if v.Hostname == host.Hostname {
+			hosts[i].IP = host.IP
+			c.Data(http.StatusOK, "text/plain", []byte("Success!"))
+		}
+	}
+	hosts = append(hosts, host)
+	c.Data(http.StatusOK, "text/plain", []byte("Success!"))
+}
+
+// ResetHosts remove all hosts data
+func ResetHosts(c *gin.Context) {
+	hosts = make([]Hosts, 0, 10)
+	c.Data(http.StatusOK, "text/plain", []byte("Hosts Reset!"))
+}
+
+// ListHosts list all hosts data
+func ListHosts(c *gin.Context) {
+	var hostValue string
+	for _, v := range hosts {
+		hostValue += fmt.Sprintf("%s\t%s", v.IP, v.Hostname)
+	}
+	c.Data(http.StatusOK, "text/plain", []byte(hostValue))
+}
