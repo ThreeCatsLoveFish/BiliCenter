ec4e282fab92511ee7d2b3415ae5057d6d765952
ThreeCats <121097854@qq.com>
2022-07-26 11:35:52 +0800

    [Feat] Update log name

--- infra/log/base.go
+++ infra/log/base.go
@@ -4,7 +4,7 @@ var w *AsyncFileWriter
 
 // Init can initialize default logger
 func init() {
-	w = NewAsyncFileWriter("output/subcenter.log")
+	w = NewAsyncFileWriter("output/bilicenter.log")
 	if err := w.initLogFile(); err != nil {
 		panic(err)
 	}
